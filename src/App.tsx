196aa0065b5f9eceed99920bc944ce29484d2230
Paul Gutsal <pavel.gutsal@gmail.com>
2023-02-17 15:45:00 +0200

    feat: git button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Segmented } from 'antd';
 import { useState } from 'react';
 import { SEGMENTS } from './constants';
+import { Address } from './modules/Address';
 import { PasswordCustom } from './modules/PasswordCustom';
 import { PasswordMeter } from './modules/PasswordMeter';
 
@@ -8,13 +9,20 @@ export const App = () => {
   const [selectedSegment, setSelectedSegment] = useState(SEGMENTS[0]);
 
   return (
-    <div className="App">
-      <Segmented
-        options={SEGMENTS}
-        onChange={(value) => setSelectedSegment(value)}
-        className="Segmented"
-      />
-      {selectedSegment === SEGMENTS[0] ? <PasswordMeter /> : <PasswordCustom />}
-    </div>
+    <>
+      <div className="App">
+        <Segmented
+          options={SEGMENTS}
+          onChange={(value) => setSelectedSegment(value)}
+          className="Segmented"
+        />
+        {selectedSegment === SEGMENTS[0] ? (
+          <PasswordMeter />
+        ) : (
+          <PasswordCustom />
+        )}
+      </div>
+      <Address />
+    </>
   );
 };
---
+++ src/modules/Address/index.tsx
@@ -0,0 +1,13 @@
+import './Address.css';
+
+export const Address = () => {
+  return (
+    <a
+      href="https://github.com/pavel-gutsal/password-meter"
+      className="Address"
+      target="blank"
+    >
+      <h1 className="Address__title">GitHub</h1>
+    </a>
+  );
+};
